8ac4baa286f24eec3ffe5ffc12acc8bb4655e381
Fabongita <nashmpiii@gmail.com>
2025-03-11 01:43:11 +0800

    revised the earlier prog 6 by changing the variable names

--- batch1_coding_standards/two_inputs_print_the_square.py
+++ batch1_coding_standards/two_inputs_print_the_square.py
@@ -1,3 +1,8 @@
 #Define a function that returns the square of the first raised to the second number
+def power(first_number,second_number):
+    return first_number**second_number
 #input 2 variables for the first and second number
+first_number = int(input("print the first number "))
+second_number = int(input("input the second number "))
 #add a print statement that returns the sqaure of the first raised to the second number
+print("the result is",power(first_number,second_number))
