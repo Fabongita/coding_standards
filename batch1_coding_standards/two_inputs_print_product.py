48930ac78c22cc6acfd18ffa95a9e42389b30ef5
Fabongita <nashmpiii@gmail.com>
2025-03-10 22:06:45 +0800

    created the function

--- batch1_coding_standards/two_inputs_print_product.py
+++ batch1_coding_standards/two_inputs_print_product.py
@@ -1,3 +1,5 @@
 #create a function that shows the product of the first and second number
+def product(first_number,second_number):
+    return first_number*second_number
 #create 2 input variables for the first number and second number
 #create a print statement that shows the result
