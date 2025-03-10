a9a4aae4fcabd97f8c58f595d04b2a712a617f02
Fabongita <nashmpiii@gmail.com>
2025-03-10 16:53:15 +0800

    fixed the naming conventions, addedthe 2 inputs, and added the print function

--- two_inputs_print_bigger_number.py
+++ two_inputs_print_bigger_number.py
@@ -1,6 +1,8 @@
 #define a function that returns the bigger number
 def checker(first_number, second_number):
-    return x if x > y else y
+    return first_number if first_number > second_number else second_number
 #ask for 2 inputs
-
+first_number = int(input("input the first number: "))
+second_number = int(input("input the second number: "))
 #prints the function
+print("bigger number is", checker(first_number,second_number))
