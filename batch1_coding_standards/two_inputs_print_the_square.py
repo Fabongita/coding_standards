9af979c2f93c624fd52308e5c937a624157e7c06
Fabongita <nashmpiii@gmail.com>
2025-03-11 01:39:51 +0800

    added pseudo-code

---
+++ batch1_coding_standards/two_inputs_print_the_square.py
@@ -0,0 +1,3 @@
+#Define a function that returns the square of the first raised to the second number
+#input 2 variables for the first and second number
+#add a print statement that returns the sqaure of the first raised to the second number
