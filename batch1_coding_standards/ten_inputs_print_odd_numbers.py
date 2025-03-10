ebc11e25fdbc292bd7863616208fbf543a52663d
Fabongita <nashmpiii@gmail.com>
2025-03-11 02:05:41 +0800

    added list comprehension input and the print statement for the result

--- batch1_coding_standards/ten_inputs_print_odd_numbers.py
+++ batch1_coding_standards/ten_inputs_print_odd_numbers.py
@@ -2,4 +2,6 @@
 def odd(numbers):
     return sum(1 for i in numbers if i % 2 != 0)
 #use a list comprehension to input the ten numbers
+numbers = [int(input("input the number "+ str(i+1) +" times:")) for i in range(10)]
 #display the number of odd numbers
+print("the odd numbers are ", odd(numbers))
