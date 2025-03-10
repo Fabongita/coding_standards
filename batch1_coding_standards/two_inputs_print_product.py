b52e57f07babef4e5781a4df33431f115cd46d76
Fabongita <nashmpiii@gmail.com>
2025-03-10 22:00:41 +0800

    added a new file and pseudo code for problem 4

---
+++ batch1_coding_standards/two_inputs_print_product.py
@@ -0,0 +1,3 @@
+#create a function that shows the product of the first and second number
+#create 2 input variables for the first number and second number
+#create a print statement that shows the result
