6a0678d3bb2fa93ecbc9397296f935d4ee0abcdc
Fabongita <nashmpiii@gmail.com>
2025-03-11 00:01:05 +0800

    changed the variable name, finished the inputs and print statement

--- batch1_coding_standards/two_inputs_print_quotient.py
+++ batch1_coding_standards/two_inputs_print_quotient.py
@@ -1,6 +1,8 @@
 # define a function for quotient
-def quotient(x,y):
-    return x/y
+def quotient(first_number,second_number):
+    return first_number/second_number
 # set up the input variables
-
+first_number = int(input("input the first number "))
+second_number = int(input("input the second number "))
 # print the quotient
+print("the quotient is", quotient(first_number,second_number))
