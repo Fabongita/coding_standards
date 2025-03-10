229d3e171054972b71dc578d445ae3117b09f631
Fabongita <nashmpiii@gmail.com>
2025-03-11 02:04:04 +0800

    added odd number function

--- batch1_coding_standards/ten_inputs_print_odd_numbers.py
+++ batch1_coding_standards/ten_inputs_print_odd_numbers.py
@@ -1,3 +1,5 @@
 #define a function that returns a sum of the odd numbers
+def odd(numbers):
+    return sum(1 for i in numbers if i % 2 != 0)
 #use a list comprehension to input the ten numbers
 #display the number of odd numbers
