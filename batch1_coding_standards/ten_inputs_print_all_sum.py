4a50d4a84979a07a466506c27b652b6ba92c3527
Fabongita <nashmpiii@gmail.com>
2025-03-11 01:57:55 +0800

    copy-pasted the code from the earlier prob 7 but changed the function name

--- batch1_coding_standards/ten_inputs_print_all_sum.py
+++ batch1_coding_standards/ten_inputs_print_all_sum.py
@@ -1,3 +1,7 @@
 #Define a function that returns the sum of all ten inputs
+def summation(numbers):
+    return sum(numbers)
 #Use list comprehension to input 10 the 10 numbers
+numbers = [int(input("input number " + str(i + 1) + " times: ")) for i in range(10)]
 #Use a print statement to show the result
+print("the sum of the 10 numbers is", summation(numbers))
