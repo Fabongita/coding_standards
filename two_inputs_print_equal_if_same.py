42e8c24ee732c1836f7d85f618438f0dc7d2724b
Fabongita <nashmpiii@gmail.com>
2025-03-10 17:06:28 +0800

    defined the function, added the inputs, and added the print statement if the numbers are or not equal

--- two_inputs_print_equal_if_same.py
+++ two_inputs_print_equal_if_same.py
@@ -1,3 +1,8 @@
 # Define a function that would check if the inputs are the same
+def Equal(x,y):
+    return "equal" if x == y else "not equal"
 # Ask for 2 inputs
+x = int(input("input 1st number "))
+y = int(input("input 2nd number "))
 #print if the numbers are same or not
+print("the two numbers are ",Equal(x,y))
