933efd0795b062a5046659512afd36b81bf8d94b
Fabongita <nashmpiii@gmail.com>
2025-03-10 21:15:17 +0800

    added function for the sum of the first and second number

--- two_inputs_print_sum.py
+++ two_inputs_print_sum.py
@@ -1,3 +1,5 @@
 #define a function that returns the sum of two numbers
+def sum(first_number,second_number):
+    return first_number + second_number
 #Make a variable for the 2 inputs
 #make a print statement that returns the function
