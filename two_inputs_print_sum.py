3c11d410da53615c726b4bf2d6e8395a1336116f
Fabongita <nashmpiii@gmail.com>
2025-03-10 21:17:11 +0800

    finished the code by adding the input variable and the print statement

--- two_inputs_print_sum.py
+++ two_inputs_print_sum.py
@@ -2,4 +2,7 @@
 def sum(first_number,second_number):
     return first_number + second_number
 #Make a variable for the 2 inputs
+first_number = int(input("input 1st number: "))
+second_number = int(input("input 2nd number: "))
 #make a print statement that returns the function
+print("the sum is ",sum(first_number,second_number))
