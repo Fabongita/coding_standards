1f326109805de6a6fd94644003fb645828b05281
Fabongita <nashmpiii@gmail.com>
2025-03-10 22:08:27 +0800

    finalized the code by adding variables and the print statement

--- batch1_coding_standards/two_inputs_print_product.py
+++ batch1_coding_standards/two_inputs_print_product.py
@@ -2,4 +2,7 @@
 def product(first_number,second_number):
     return first_number*second_number
 #create 2 input variables for the first number and second number
+first_number = int(input("input 1st number: "))
+second_number = int(input("input 2nd number: "))
 #create a print statement that shows the result
+print("the product is", product(first_number,second_number))
