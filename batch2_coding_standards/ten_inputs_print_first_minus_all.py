45dc712ca4182e1063b11164bb8f3b6ec8c3baf1
Fabongita <nashmpiii@gmail.com>
2025-03-11 15:08:02 +0800

    added input variables, for loop, and print statement

--- batch2_coding_standards/ten_inputs_print_first_minus_all.py
+++ batch2_coding_standards/ten_inputs_print_first_minus_all.py
@@ -1,5 +1,9 @@
 #Create a list comprehension input variable that has 10 inputs
-
+numbers = [int(input("input number "+str(i+1)+":"))for i in range(10)]
+first_number = numbers[0]
 #use a for loop that starts with the second number to the last number that subtracts itself from the 1st number
-
+for i in numbers[1:]:
+    first_number -= i
+    result = first_number
 #print the statement
+print(result)
