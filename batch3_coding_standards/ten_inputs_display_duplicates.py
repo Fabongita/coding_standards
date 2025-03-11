7e74e585ef1c36b112f7be58c225a1305b532b91
Fabongita <nashmpiii@gmail.com>
2025-03-12 01:16:43 +0800

    converted the function logic into a normal for loop to get the logic right

--- batch3_coding_standards/ten_inputs_display_duplicates.py
+++ batch3_coding_standards/ten_inputs_display_duplicates.py
@@ -1,4 +1,4 @@
-# Add a function that returns the numbers that have duplicates
+"""# Add a function that returns the numbers that have duplicates
 def duplicates(numbers):
     return [x for x in numbers if numbers.count(x) == 1]
 # Add an input variable that asks for 10 numbers
@@ -6,6 +6,11 @@ numbers = [int(input("input the number "+ str(i+1)+ ":" ))for i in range(10)]
 
 #  Add the print statement that displays the numbers
 print("the duplicates are ", duplicates(numbers))
-
-
-    
+"""
+x = []
+numbers = [int(input("input the number "+ str(i+1)+ ":" ))for i in range(10)]
+for i in numbers:
+    if numbers.count(i) == 1:
+        x.append(i)
+print(f"the duplicates are {x}")
+        
