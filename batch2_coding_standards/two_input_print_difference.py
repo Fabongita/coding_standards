64cb895cf97ad46d8d761d42f88484376933c5a4
Fabongita <nashmpiii@gmail.com>
2025-03-11 14:28:08 +0800

    finished the code by adding the input variables and print statement following snake case

--- batch2_coding_standards/two_input_print_difference.py
+++ batch2_coding_standards/two_input_print_difference.py
@@ -1,2 +1,5 @@
 #add 2 input variables 
+first_number = int(input("input the first number: "))
+second_number = int(input("input the 2nd number: "))
 # add print statement
+print(first_number-second_number)
