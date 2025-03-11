448c183863daf47602e84c1eedc853ff1c22c2d7
Fabongita <nashmpiii@gmail.com>
2025-03-11 15:24:26 +0800

    added variable, added while loop

--- batch2_coding_standards/print_odd_1_to_100.py
+++ batch2_coding_standards/print_odd_1_to_100.py
@@ -1,2 +1,6 @@
 # initiatilze a variable that starts from 1 
+starting_number = 1
 # use a while loop that ends to 100 then prints out the odd numbers
+while starting_number <= 100:
+    print(starting_number)
+    starting_number += 2
