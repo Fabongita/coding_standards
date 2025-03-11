9b13a4bc992a6d8fb33c5cad871dc11199d7d6e3
Fabongita <nashmpiii@gmail.com>
2025-03-11 14:32:24 +0800

    added pseudocode, input variables, and print statement with floor division

---
+++ batch2_coding_standards/two_input_print_quotient_no_decimal.py
@@ -0,0 +1,5 @@
+#add two input variables for the 1st and 2nd number
+first_number = int(input("input the first number: "))
+second_number = int(input("input the second number: "))
+#print a floor division between the two numbers
+print(first_number//second_number)
