53a281a03db84cf6e426dc30d9d9b031948273ea
Fabongita <nashmpiii@gmail.com>
2025-03-11 14:39:44 +0800

    added pseudo-code, the two input variables, and modulo print statement.

---
+++ batch2_coding_standards/two_inputs_print_remainder.py
@@ -0,0 +1,5 @@
+#Create two input variables for the first and second number 
+first_number = int(input("print the 1st number: "))
+second_number = int(input("print the 2nd number: "))
+#Print a modulo statement between the first number and second number respectively
+print(first_number%second_number)
