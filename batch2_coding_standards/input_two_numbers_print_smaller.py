554e863c6e1f670f11e3cd344d10adb12375e0dc
Fabongita <nashmpiii@gmail.com>
2025-03-11 14:16:16 +0800

    Added the variables and fixed the names, added the print statement

--- batch2_coding_standards/input_two_numbers_print_smaller.py
+++ batch2_coding_standards/input_two_numbers_print_smaller.py
@@ -1,2 +1,5 @@
 #add 2 variables for the 1st and 2nd number
+first_number = int(input("input the 1st number: "))
+second_number = int(input("input the 2nd number: "))
 #Use print logic to print out the smaller number
+print("the smaller number is:",first_number if first_number < second_number else second_number)
