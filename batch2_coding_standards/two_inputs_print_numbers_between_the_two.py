d944b8a4d8613c504222304399096e944874ee15
Fabongita <nashmpiii@gmail.com>
2025-03-11 15:51:29 +0800

    added the function, variables, print statement, changed the variable name that is appropriate to the coding standards

--- batch2_coding_standards/two_inputs_print_numbers_between_the_two.py
+++ batch2_coding_standards/two_inputs_print_numbers_between_the_two.py
@@ -1,3 +1,12 @@
 #create a function that returns the numbers between the first number and the second number
+def between(first_number, second_number):
+    if first_number < second_number:
+        return range(first_number + 1, second_number)
+    else:
+        return range(second_number + 1, first_number)
 #Initialize two input variables for the 1st and 2nd number
+first_number = int(input("Input the first number: "))
+second_number = int(input("Input the second number: "))
+
 #print the numbers
+print(f"The numbers between {first_number} and {second_number} are: {list(between(first_number, second_number))}")
