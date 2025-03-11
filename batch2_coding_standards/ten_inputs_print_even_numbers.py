624f0f7642a8c4d7089efc2452cef25604160fd8
Fabongita <nashmpiii@gmail.com>
2025-03-11 15:13:43 +0800

    Added the even number function, input variable, print statement

--- batch2_coding_standards/ten_inputs_print_even_numbers.py
+++ batch2_coding_standards/ten_inputs_print_even_numbers.py
@@ -1,5 +1,7 @@
 #Add a function that sums up the number of even numbers
-
+def even(numbers):
+    return(sum(1 for i in numbers if i % 2 == 0))
 #Add an input variable for the 10 numbers
-
+numbers =[int(input("input number"+str(i+1)+":"))for i in range(10)]
 #print statement for the results
+print("the number of even numbers are", even(numbers))
