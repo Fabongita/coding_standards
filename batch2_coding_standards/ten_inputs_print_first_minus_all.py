7ffdb66e00b9917b790a0c79436add6f2d21afd2
Fabongita <nashmpiii@gmail.com>
2025-03-11 15:06:09 +0800

    Added file for prog 6 and added pseudo-code

---
+++ batch2_coding_standards/ten_inputs_print_first_minus_all.py
@@ -0,0 +1,5 @@
+#Create a list comprehension input variable that has 10 inputs
+
+#use a for loop that starts with the second number to the last number that subtracts itself from the 1st number
+
+#print the statement
