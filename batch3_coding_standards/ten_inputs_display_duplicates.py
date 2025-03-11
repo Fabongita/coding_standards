6cc73ca5c5aea159b31977dd57646ec6105531d7
Fabongita <nashmpiii@gmail.com>
2025-03-12 00:45:58 +0800

    added the variables, function, and print statement but the function doesn't work as intended

--- batch3_coding_standards/ten_inputs_display_duplicates.py
+++ batch3_coding_standards/ten_inputs_display_duplicates.py
@@ -1,3 +1,8 @@
 # Add a function that returns the numbers that have duplicates
+def duplicates(numbers):
+    return [x for i in numbers if i in numbers == True]
 # Add an input variable that asks for 10 numbers
-# Add the print statement that displays the numbers
+numbers = [int(input("input the number "+ str(i+1)+ ":" ))for i in range(10)]
+
+#  Add the print statement that displays the numbers
+print("the duplicates are ", duplicates(numbers))
