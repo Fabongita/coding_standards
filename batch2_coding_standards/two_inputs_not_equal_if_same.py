21ea557f617b2b8c4d54af2e7082abbafed5db42
Fabongita <nashmpiii@gmail.com>
2025-03-11 14:23:51 +0800

    changed the variable names that fits the coding standard

--- batch2_coding_standards/two_inputs_not_equal_if_same.py
+++ batch2_coding_standards/two_inputs_not_equal_if_same.py
@@ -1,5 +1,5 @@
 #Add two variables for 1st and second number
-x = int(input("input the 1st number: "))
-y = int(input("input the second number: "))
+first_number = int(input("input the 1st number: "))
+second_number = int(input("input the second number: "))
 #add a print logic statement that says if the first and second number are equal 
-print(("equal" if x == y else "not equal"))
+print(("equal" if first_number == second_number else "not equal"))
