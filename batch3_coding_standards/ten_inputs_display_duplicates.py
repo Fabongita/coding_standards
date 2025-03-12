c92d8abe94d861c8789553dd8680ffd6b1578561
Fabongita <nashmpiii@gmail.com>
2025-03-12 11:45:13 +0800

    changed the input variable into something more simple

--- batch3_coding_standards/ten_inputs_display_duplicates.py
+++ batch3_coding_standards/ten_inputs_display_duplicates.py
@@ -1,11 +1,13 @@
-"""# Add a function that returns the numbers that have duplicates
+# Add a function that returns the numbers that have duplicates
 def duplicates(numbers):
-    return [x for x in numbers if numbers.count(x) == 1]
+    x = []
+    return [i for i in numbers if numbers.count(i) > 1 and i not in x]
 # Add an input variable that asks for 10 numbers
-numbers = [int(input("input the number "+ str(i+1)+ ":" ))for i in range(10)]
+numbers = list(map(int, input("Enter 10 numbers seperated by space: ").split()))
 
 #  Add the print statement that displays the numbers
 print("the duplicates are ", duplicates(numbers))
+
 """
 x = []
 numbers = [int(input("input the number "+ str(i+1)+ ":" ))for i in range(10)]
@@ -15,4 +17,4 @@ for i in numbers:
     
     
 print(f"the duplicates are {x}")
-        
+"""        
