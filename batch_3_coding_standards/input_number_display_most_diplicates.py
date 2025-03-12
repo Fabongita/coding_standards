9b0eead3d7e48fff8fd70540531755bddbb6120c
Fabongita <nashmpiii@gmail.com>
2025-03-12 13:48:42 +0800

    fixed the try and except logic

--- batch_3_coding_standards/input_number_display_most_diplicates.py
+++ batch_3_coding_standards/input_number_display_most_diplicates.py
@@ -1,13 +1,14 @@
 #initialize variables for the list of numbers and the number with the most amount of duplicates
 numbers = []
-most_duplicates = ()
+most_duplicates = None
 #Use a while loop to ask for user number inputs which displays the most duplicates if user input is invalid
 while True:
-    user_input = int(input("input numbers (input non-numbers if you want to stop): "))
+    user_input = input("input numbers (input non-numbers if you want to stop): ")
     try:
-        numbers.append(user_input)
+        int_converter = int(user_input)
+        numbers.append(int_converter)
     except ValueError:
-        break
-
+       break
+print(numbers)
 
 
