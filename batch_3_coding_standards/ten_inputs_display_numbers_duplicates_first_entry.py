1384304b47ef7771afb5763a59c0fc702d184cce
Fabongita <nashmpiii@gmail.com>
2025-03-12 21:00:59 +0800

    revised the for loop logic and revised the print statement

--- batch_3_coding_standards/ten_inputs_display_numbers_duplicates_first_entry.py
+++ batch_3_coding_standards/ten_inputs_display_numbers_duplicates_first_entry.py
@@ -3,5 +3,9 @@ all_numbers_with_duplicates = []
 input_numbers = [int(input(f"input numbers {i+1} times : "))for i in range(10)]
 #use a for loop to go through the input numbers and determine if it is a duplicate or not
 for i in input_numbers:
+    if input_numbers.count(i) > 1 and i not in all_numbers_with_duplicates:
+        all_numbers_with_duplicates.append(i)
     if input_numbers.count(i) == 1:
-        all_numbers_no_duplicates.append(i)
+        all_numbers_with_duplicates.append(i)
+print(f"all of the numbers with duplicates first entry are {all_numbers_with_duplicates}")
+
