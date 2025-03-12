4131336ada37cb8baf979ba96a3e1cfb5d86869e
Fabongita <nashmpiii@gmail.com>
2025-03-12 12:22:18 +0800

    Fixed the logic for the function, and deleted the comments

--- batch3_coding_standards/ten_inputs_display_duplicates.py
+++ batch3_coding_standards/ten_inputs_display_duplicates.py
@@ -1,20 +1,8 @@
 # Add a function that returns the numbers that have duplicates
 def duplicates(numbers):
-    x = []
-    return [i for i in numbers if numbers.count(i) > 1 and i not in x]
+    return [i for index, i in enumerate(numbers) if numbers.count(i) > 1 and i not in numbers[:index]]
 # Add an input variable that asks for 10 numbers
-numbers = list(map(int, input("Enter 10 numbers seperated by space: ").split()))
+numbers = list(map(int, input("Enter 10 numbers seperated by comma: ").split(",")))
 
 #  Add the print statement that displays the numbers
 print("the duplicates are ", duplicates(numbers))
-
-"""
-x = []
-numbers = [int(input("input the number "+ str(i+1)+ ":" ))for i in range(10)]
-for i in numbers:
-    if numbers.count(i) > 1 and i not in x:
-        x.append(i)
-    
-    
-print(f"the duplicates are {x}")
-"""        
