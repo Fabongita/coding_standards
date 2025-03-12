5aec6b05c83ba54975b7395df2180044a6e1ae06
Fabongita <nashmpiii@gmail.com>
2025-03-12 18:24:34 +0800

    added sorted function and used reverse = true

--- batch_3_coding_standards/input_number_display_highest_to_lowest.py
+++ batch_3_coding_standards/input_number_display_highest_to_lowest.py
@@ -1,6 +1,6 @@
 #initialize a variable for the numbers, and the highest number
 numbers = []
-highest_number = None
+highest_number_to_lowest_number = None
 #use a while loop and try and except for the input
 while True:
     input_numbers = input("input a number (input a non-number if you want to stop): ")
@@ -10,3 +10,9 @@ while True:
     except ValueError:
         break
 #use an if statement to determine the highest number and display it
+if numbers:
+    highest_number_to_lowest_number = sorted(numbers,reverse=True)
+    print(f"the numbers from highest to lowest are {highest_number_to_lowest_number}")
+else:
+    print("no number inputted")
+    
