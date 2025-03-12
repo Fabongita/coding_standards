d648cb1ef72a35bb554233770dcb63d04b9f4f05
Fabongita <nashmpiii@gmail.com>
2025-03-12 21:37:46 +0800

    Added pseudo-code, used while loops from batch 4 programs as reference, but still getting TypeErrors

--- batch_3_coding_standards/input_number_display_duplicate_or_unique.py
+++ batch_3_coding_standards/input_number_display_duplicate_or_unique.py
@@ -1,2 +1,14 @@
 #initialize a list variable for all of the numbers
+all_numbers = []
 #use a while loop for the input, displaying of "Unique" or "Duplicate"
+while True:
+    number_input = (input("Input a number: " ))
+    try:
+        all_numbers.append(number_input)
+        for i in number_input:
+            if number_input.count(i) > 1 and i not in all_numbers:
+                print("This numbe is unique :)")
+            if number_input(i) == 1 and i in all_numbers:
+                print("This number is a duplicate :(")
+    except ValueError:
+        break
