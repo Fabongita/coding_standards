af8b691acb6fa0a2a9d0f126088d29693aadd51f
Fabongita <nashmpiii@gmail.com>
2025-03-12 17:59:27 +0800

    added pseudo-code

---
+++ batch_3_coding_standards/input_number_display_highest_number.py
@@ -0,0 +1,5 @@
+#initialize a variable for the numbers, and the highest number
+
+#use a while loop and try and except for the input
+
+#use an if statement to determine the highest number and display it
