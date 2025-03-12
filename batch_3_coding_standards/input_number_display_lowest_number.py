6690888e4fe49deabfd8912e2976ef0be2d3afab
Fabongita <nashmpiii@gmail.com>
2025-03-12 22:41:08 +0800

    Copy pasted the entire code from problem 3 batch 4 because it was just the same problem but with different variable names. and replaced max() function with min() function

---
+++ batch_3_coding_standards/input_number_display_lowest_number.py
@@ -0,0 +1,17 @@
+#initialize a variable for the numbers, and the highest number
+numbers = []
+lowest_number = None
+#use a while loop and try and except for the input
+while True:
+    input_numbers = input("input a number (input a non-number if you want to stop): ")
+    try:
+        convert_input_numbers_integer = int(input_numbers)
+        numbers.append(input_numbers)
+    except ValueError:
+        break
+#use an if statement to determine the highest number and display it
+if numbers:
+    lowest_number = min(numbers)
+    print(f"the lowest number is",*lowest_number)
+else:
+    print("there is no number inputted")
