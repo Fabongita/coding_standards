f9c24d1c5ba02053422ec3efdfa4cfe119d27dae
Fabongita <nashmpiii@gmail.com>
2025-03-12 16:51:13 +0800

    added the if logic

--- batch_3_coding_standards/input_number_display_most_diplicates.py
+++ batch_3_coding_standards/input_number_display_most_diplicates.py
@@ -1,7 +1,7 @@
 #initialize variables for the list of numbers and the number with the most amount of duplicates
 numbers = []
 most_duplicates = None
-#Use a while loop to ask for user number inputs which displays the most duplicates if user input is invalid
+#Use a while loop to ask for user number inputs then stops if it has a valueError
 while True:
     user_input = input("input numbers (input non-numbers if you want to stop): ")
     try:
@@ -9,6 +9,13 @@ while True:
         numbers.append(int_converter)
     except ValueError:
        break
-print(numbers)
+# An if statement that checks if the number is a duplicate and the most amount of duplicate
+if numbers:
+    max_count = max(numbers.count(n) for n in numbers)
+    most_duplicates = list(set(n for n in numbers if numbers.count(n) == max_count))
+    print("Numbers entered:", numbers)
+    print("Most duplicated number(s):", most_duplicates)
+else:
+    print("No numbers were entered.")
 
 
