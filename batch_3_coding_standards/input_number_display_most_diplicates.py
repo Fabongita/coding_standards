ab317de7bc4d94e4abb94fb12a129ebfb0ef276b
Fabongita <nashmpiii@gmail.com>
2025-03-12 13:43:05 +0800

    added try and except for appending the list of numbers and  breaking the loop if it has a valueError

--- batch_3_coding_standards/input_number_display_most_diplicates.py
+++ batch_3_coding_standards/input_number_display_most_diplicates.py
@@ -1,7 +1,13 @@
+#initialize variables for the list of numbers and the number with the most amount of duplicates
+numbers = []
+most_duplicates = ()
 #Use a while loop to ask for user number inputs which displays the most duplicates if user input is invalid
 while True:
     user_input = int(input("input numbers (input non-numbers if you want to stop): "))
-    numbers = []
-    numbers.append(user_input)
-    most_duplicates = []
-    if user_input 
+    try:
+        numbers.append(user_input)
+    except ValueError:
+        break
+
+
+
