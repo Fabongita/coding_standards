0c80b8fcb6cc0a69a95471934b8c30678ae4b06c
Fabongita <nashmpiii@gmail.com>
2025-03-12 20:48:34 +0800

    reverted the old input variable that uses for loops instead of split()

--- batch_4_coding_standards/ten_inputs_display_duplicates.py
+++ batch_4_coding_standards/ten_inputs_display_duplicates.py
@@ -2,7 +2,7 @@
 def duplicates(numbers):
     return [i for index, i in enumerate(numbers) if numbers.count(i) > 1 and i not in numbers[:index]]
 # Add an input variable that asks for 10 numbers
-numbers = list(map(int, input("Enter 10 numbers seperated by comma: ").split(",")))
+numbers = [int(input(f"input numbers {i+1} times : "))for i in range(10)]
 
 #  Add the print statement that displays the numbers
 print("the duplicates are ", duplicates(numbers))
