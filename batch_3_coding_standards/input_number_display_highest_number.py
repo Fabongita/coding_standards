bdcdde72227a658f404f46511d52d7791362229a
Fabongita <nashmpiii@gmail.com>
2025-03-12 18:08:44 +0800

    finished adding the variables, while loop, try and except, and if statement,

--- batch_3_coding_standards/input_number_display_highest_number.py
+++ batch_3_coding_standards/input_number_display_highest_number.py
@@ -1,5 +1,17 @@
 #initialize a variable for the numbers, and the highest number
-
+numbers = []
+highest_number = None
 #use a while loop and try and except for the input
-
+while True:
+    input_numbers = input("input a number (input a non-number if you want to stop): ")
+    try:
+        convert_input_numbers_integer = int(input_numbers)
+        numbers.append(input_numbers)
+    except ValueError:
+        break
 #use an if statement to determine the highest number and display it
+if numbers:
+    highest_number = max(numbers)
+    print(f"the highest number is {highest_number}")
+else:
+    print("there is no number inputted")
