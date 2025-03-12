c5fbd33214af8a2c266507747929d2d4dbf4463c
Fabongita <nashmpiii@gmail.com>
2025-03-12 22:25:53 +0800

    added for loops inside the while loop that checks if i is equa l to the input and used the logic for finding duplicates

--- batch_3_coding_standards/input_number_display_duplicate_or_unique.py
+++ batch_3_coding_standards/input_number_display_duplicate_or_unique.py
@@ -1,14 +1,17 @@
-#initialize a list variable for all of the numbers
-all_numbers = []
-#use a while loop for the input, displaying of "Unique" or "Duplicate"
-while True:
-    number_input = (input("Input a number: " ))
-    try:
+try:
+    #initialize a list variable for all of the numbers
+    all_numbers = []
+    #use a while loop for the input, displaying of "Unique" or "Duplicate"
+    while True:
+        number_input = int(input("Input a number: " ))
         all_numbers.append(number_input)
-        for i in number_input:
-            if number_input.count(i) > 1 and i not in all_numbers:
-                print("This numbe is unique :)")
-            if number_input(i) == 1 and i in all_numbers:
-                print("This number is a duplicate :(")
-    except ValueError:
-        break
+        for i in all_numbers:
+            if i == number_input:
+                if all_numbers.count(i) > 1:
+                    print("This number is duplicate :(")
+                    break
+                else:
+                    print("This number is unique :)")
+                    break
+except ValueError:
+    pass
